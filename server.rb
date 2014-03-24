a9a95ab2316f23461d807b15f4affdaa9e662f4b
Andrew <aj.cumine@btinternet.com>
2014-03-24 16:21:39 +0000

    added tags

--- server.rb
+++ server.rb
@@ -3,6 +3,7 @@ require 'data_mapper'
 env = ENV["RACK_ENV"] || "developement"
 DataMapper.setup(:default, "postgres://localhost/bookmark_manager_#{env}")
 require './lib/link'
+require './lib/tag'
 DataMapper.finalize
 DataMapper.auto_upgrade!
 
@@ -14,6 +15,9 @@ end
 post '/links' do
   url = params['url']
   title = params['title']
-  Link.create(:url => url, :title => title)
+  tags = params['tags'].split(' ').map do |tag|
+    Tag.first_or_create(:text => tag)
+  end
+  Link.create(:url => url, :title => title, :tags => tags)
   redirect to('/')
 end
